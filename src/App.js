62c5ba532a698758d0e37c75d7cfa3ec17c3bc59
O-Robot <adewaleogooluwani@gmail.com>
2023-07-09 23:39:18 +0100

    login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Login from './components/login'
-import SignUp from './components/signup'
-import FixedBottomNavigation from './components/navigation'
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import SignUp from "./components/signup";
+
+import FixedBottomNavigation from "./components/navigation";
+import Login from "./pages/Auth/login";
 
 function App() {
   return (
@@ -12,21 +14,20 @@ function App() {
       {/* <h1 className='float-left container'>Studentity</h1> */}
       {/* <h1 className='float-right'>Login</h1> */}
       <div className="App">
-        
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
           <div className="container">
-            <Link className="navbar-brand" to={'/sign-in'}>
+            <Link className="navbar-brand" to={"/sign-in"}>
               Studentity
             </Link>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
+                  <Link className="nav-link" to={"/sign-in"}>
                     Login
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
+                  <Link className="nav-link" to={"/sign-up"}>
                     Sign up
                   </Link>
                 </li>
@@ -35,17 +36,17 @@ function App() {
           </div>
         </nav>
         {/* <div className="auth-wrapper"> */}
-          {/* <div className="auth-inner"> */}
-            <Routes>
-              <Route exact path="/" element={<Login />} />
-              <Route path="/sign-in" element={<Login />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/home" element={<FixedBottomNavigation />}/>
-            </Routes>
-          {/* </div> */}
+        {/* <div className="auth-inner"> */}
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/sign-in" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/home" element={<FixedBottomNavigation />} />
+        </Routes>
         {/* </div> */}
-       </div>
+        {/* </div> */}
+      </div>
     </Router>
-  )
+  );
 }
-export default App
+export default App;
---
+++ src/pages/Auth/signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./auth.css";
+
+const signup = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default signup
