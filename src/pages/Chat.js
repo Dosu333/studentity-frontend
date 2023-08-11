049f87c286133a222ce86f0a2f91277570b10f8c
Fawaz Oduola <oduolafawaz@gmail.com>
2023-08-11 13:44:52 +0100

    added AI typing animation and also prevent user from sending message before AI replies

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,9 +1,13 @@
 import { red } from "@mui/material/colors";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import avatar from "../components/7.png";
 import avatar2 from "../components/6.png";
+import Typing from "../components/Typing";
+
 
 const Chat = () => {
+  const messagesEndRef = useRef(null);
+  const [isTyping, setIsTyping] = useState(false);
   const [textareaValue, setTextareaValue] = useState('');
   const [messages, setMessage] = useState([
     {
@@ -12,16 +16,39 @@ const Chat = () => {
     }
   ])
 
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(scrollToBottom, [messages]);
+  
+  
   function handleSubmit(){
+    if (isTyping || textareaValue===""){
+      return
+    }
+    scrollToBottom()
     console.log(textareaValue)
-    setMessage([...messages, {
-      message: textareaValue,
-      sender: "user"
-    }, {
+    messages.push(
+      {
+        message: textareaValue,
+        sender: "user"
+      }
+    )
+    setMessage(messages)
+    getAIresponse()
+    setTextareaValue("")
+
+  }
+
+  function getAIresponse(){
+    setIsTyping(true)
+    messages.push({
       message: "AI response",
       sender: "AI"
-    }])
-    setTextareaValue("")
+    })
+    setMessage(messages)
+    
 
   }
   return (
@@ -34,30 +61,33 @@ const Chat = () => {
       <div class="card-body chatB">
         <div className="chatInside">
           {/* chats Start  */}
+          
           {
             messages.map((message, i) => (
-              <div className={message.sender === "AI" ? "chatLeft" : "chatRight"}>
-              <div className="image">
-                <img src={message.sender==="AI"?avatar2:null} alt="" className="img-avatar" />
-              </div>
-              <div className={message.sender === "AI" ? "aBody" : "uBody"}>
-                <div className={message.sender === "AI" ? "aContent" : "uContent"}>
-                  {message.message}
+              <div className={message.sender === "AI" ? "chatLeft" : "chatRight"} style={i===messages.length-1?{marginBottom: "200px"}:null}>
+                  <div className="image">
+                    <img src={message.sender==="AI"?avatar2:null} alt="" className="img-avatar" />
+                  </div>
+                <div className={message.sender === "AI" ? "aBody" : "uBody"}>
+                  <div className={message.sender === "AI" ? "aContent" : "uContent"}>
+                    <div> { isTyping && i === messages.length - 1 && message.sender==="AI" ? <Typing /> : message.message } </div>                  
+                  </div>
+                  <small className={message.sender==="AI"?"chat-date": "uchat-date"}>
+                    <i className={message.sender==="AI" ? "tf-icons bx bx-calendar":" bx bx-calendar"}></i> 22-07-2023 at 09:00
+                  </small>
                 </div>
-                <small className={message.sender==="AI"?"chat-date": "uchat-date"}>
-                  <i className={message.sender==="AI" ? "tf-icons bx bx-calendar":" bx bx-calendar"}></i> 22-07-2023 at 09:00
-                </small>
+                <div className="image">
+                  <img src={message.sender === "user" ? avatar : null} alt="" className="img-avatar" />
+                </div>
               </div>
-              <div className="image">
-              <img src={message.sender === "user" ? avatar : null} alt="" className="img-avatar" />
-            </div>
-            </div>
             ))
           }
+          <div ref={messagesEndRef}/>
+          <div  />
           
           {/* chats End */}
         </div>
-
+        
         {/* SendArea  */}
         <div class=" byr">
           <div className="add">
@@ -84,7 +114,7 @@ const Chat = () => {
               style={{ padding: "30px 0" }}
               onClick={handleSubmit}
             >
-              <span class="tf-icons bx bx-send"></span>
+              <span class={isTyping?"tf-icons bx bx-stop":"tf-icons bx bx-send"} style={{fontSize: '32px'}}></span>
             </button>
           </div>
         </div>
