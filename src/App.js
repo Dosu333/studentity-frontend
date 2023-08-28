c1da3ce747a78305b6cbe988fc3c49bc11a4bfaf
Fawaz Oduola <oduolafawaz@gmail.com>
2023-08-28 09:41:14 +0100

    testing the hide bottom nav on mobile

--- src/App.js
+++ src/App.js
@@ -45,9 +45,9 @@ function App() {
           </div>{" "}
         </div>
       </div>
-      <Headroom>
+      {/* <Headroom> */}
         <Bottomnav />
-      </Headroom>
+      {/* </Headroom> */}
       
     </Router>
   );
